e1f9b3a7e4246120feb158e9ad03ae035979c285
Ali506108 <aliduisen77@gmail.com>
2026-04-16 12:02:20 +0500

    new changes

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module awesomeProject3
 
 go 1.25
 
-
+require github.com/google/uuid v1.6.0 // indirect
--- routine/goroutine.go
+++ routine/goroutine.go
@@ -1,17 +1,30 @@
 package routine
 
-import (
-	"fmt"
-)
+import "github.com/google/uuid"
 
-func concurency_main() {
+type Compony struct {
+	id             string
+	name           string
+	founder        string
+	avg_make_money float64
+	country        []Country
+}
 
-	fmt.Println("Hello world!")
+type Country struct {
+	id   string
+	name string
+}
 
-	country := make(map[string]string)
+func newCompony(name string, name_of_compony string, founder string, avg_make_money float64, my_country Country) Compony {
 
-	country["U.K"] = "London"
-
-	fmt.Println(country)
+	return Compony{
+		id:             uuid.New().String(),
+		name:           name_of_compony,
+		founder:        founder,
+		avg_make_money: avg_make_money,
+		country:        []Country{my_country},
+	}
 
 }
+
+// we have table of facts and измерения
